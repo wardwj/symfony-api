792f304b8c170c5350af7c70878804b332005b3a
William Ward <william.ward074@gmail.com>
2021-10-21 16:14:38 +0100

    Create BlogController with basic routes

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/blog")
+ */
+class BlogController extends AbstractController
+{
+
+	private const POSTS = [
+		[
+			'id'	=> 1,
+			'slug'	=> 'hello-world',
+			'title'	=> 'Hello World!'
+		],
+		[
+			'id'	=> 2,
+			'slug'	=> 'another-post',
+			'title'	=> 'Another example!'
+		],
+		[
+			'id'	=> 3,
+			'slug'	=> 'last-example',
+			'title'	=> 'Last example!'
+		]
+	];
+
+    /**
+     * @Route("/", name="blog_list")
+     */
+    public function list(): JsonResponse
+    {
+    	return new JsonResponse(self::POSTS);
+    }
+
+    /**
+     * @Route("/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     */
+    public function post($id): JsonResponse
+    {
+    	return new JsonResponse(
+    		self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
+    	);
+    }
+
+    /**
+     * @Route("/{slug}", name="blog_by_slug")
+     */
+    public function postBySlug($slug): JsonResponse
+    {
+    	return new JsonResponse(
+    		self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
+    	);
+    }
+}
