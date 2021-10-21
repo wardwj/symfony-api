36d8e0799d4a62999fad7992fce3574f177f8626
William Ward <william.ward074@gmail.com>
2021-10-21 17:04:30 +0100

    Find blog posts through database

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,59 +2,71 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\BlogPost;
 
 /**
  * @Route("/blog")
  */
 class BlogController extends AbstractController
 {
-
-	private const POSTS = [
-		[
-			'id'	=> 1,
-			'slug'	=> 'hello-world',
-			'title'	=> 'Hello World!'
-		],
-		[
-			'id'	=> 2,
-			'slug'	=> 'another-post',
-			'title'	=> 'Another example!'
-		],
-		[
-			'id'	=> 3,
-			'slug'	=> 'last-example',
-			'title'	=> 'Last example!'
-		]
-	];
-
     /**
-     * @Route("/", name="blog_list")
+     * @Route("/{page}", name="blog_list", defaults={"page":5}, requirements={"page"="\d+"})
      */
-    public function list(): JsonResponse
+    public function list($page = 1, Request $request)
     {
-    	return new JsonResponse(self::POSTS);
-    }
+    	$limit = $request->get('limit', 10);
 
-    /**
-     * @Route("/{id}", name="blog_by_id", requirements={"id"="\d+"})
-     */
-    public function post($id): JsonResponse
-    {
-    	return new JsonResponse(
-    		self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
+    	$repository = $this->getDoctrine()->getRepository(BlogPost::class);
+        $items = $repository->findAll();
+
+    	return $this->json(
+    		[
+    			'page' => $page,
+    			'limit' => $limit,
+    			'data' => array_map(function (BlogPost $item) {
+    				return $this->generateUrl('blog_by_slug', ['slug'=> $item->getSlug()]);
+    			}, self::POSTS)
+    		]
     	);
     }
 
     /**
-     * @Route("/{slug}", name="blog_by_slug")
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
      */
-    public function postBySlug($slug): JsonResponse
+    public function post($id, Request $request)
     {
-    	return new JsonResponse(
-    		self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
+    	return $this->json(
+	    	$this->getDoctrine()->getRepository(BlogPost::class)->find($id)
+	    );
+    }
+
+    /**
+     * @Route("/post/{slug}", name="blog_by_slug")
+     */
+    public function postBySlug($slug, Request $request)
+    {
+    	return $this->json(
+	    	$this->getDoctrine()->getRepository(BlogPost::class)->findOneBy(['slug' => $slug])
     	);
+    }
+
+    /**
+     * @Route("/add", name="blog_add", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+    	$serializer = $this->get('serializer');
+
+    	$blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+
+    	$em = $this->getDoctrine()->getManager();
+    	$em->persist($blogPost);
+    	$em->flush();
+
+    	return $this->json($blogPost);
     }
 }
