a2d5b3d863bd3d77e01b195af19a7cb894e1e4d1
William Ward <william.ward074@gmail.com>
2021-11-11 17:47:29 +0000

    Include faker bundle and generate additional content

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,11 +2,13 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\BlogPost;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
@@ -15,44 +17,62 @@ class AppFixtures extends Fixture
      */
     private $passwordHasher;
 
+    /**
+     * @var Factory
+     */
+    private $faker;
+
     public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         $this->passwordHasher = $passwordHasher;
+        $this->faker = Factory::create();
     }
     public function load(ObjectManager $manager): void
     {
         $this->loadUsers($manager);
         $this->loadBlogPosts($manager);
+        $this->loadComments($manager);
     }
 
     public function loadBlogPosts(ObjectManager $manager)
     {
+
         $user = $this->getReference('user_admin');
 
-        $blogPost = new BlogPost();
-        $blogPost->setTitle("A first post!");
-        $blogPost->setPublished(new \DateTime('2021-10-01 12:00:00'));
-        $blogPost->setContent("Post text!");
-        $blogPost->setAuthor($user);
-        $blogPost->setSlug("a-first-post");
+        for ($i = 0; $i < 100; $i++)
+        {
+            $blogPost = new BlogPost();
+            $blogPost->setTitle($this->faker->realText(30));
+            $blogPost->setPublished($this->faker->dateTime);
+            $blogPost->setContent($this->faker->realText());
+            $blogPost->setAuthor($user);
+            $blogPost->setSlug($this->faker->slug);
 
-        $manager->persist($blogPost);
+            $this->setReference("blog_post_$i", $blogPost);
 
-        $blogPost = new BlogPost();
-        $blogPost->setTitle("A second post!");
-        $blogPost->setPublished(new \DateTime('2021-10-02 12:00:00'));
-        $blogPost->setContent("Post text!");
-        $blogPost->setAuthor($user);
-        $blogPost->setSlug("a-second-post");
-
-        $manager->persist($blogPost);
+            $manager->persist($blogPost);
+        }
 
         $manager->flush();
     }
 
     public function loadComments(ObjectManager $manager)
     {
+        for ($i = 0; $i < 100; $i++)
+        {
+            for ($j = 0; $j < rand(1,10); $j++)
+            {
+                $comment = new Comment();
+                $comment->setContent($this->faker->realText());
+                $comment->setPublished($this->faker->dateTimeThisYear);
+                $comment->setAuthor($this->getReference('user_admin'));
+                $comment->setBlogPost($this->getReference("blog_post_$i"));
 
+                $manager->persist($comment);
+            }
+        }
+
+        $manager->flush();
     }
 
     public function loadUsers(ObjectManager $manager)
